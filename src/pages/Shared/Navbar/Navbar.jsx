fb3abe99f60afe0d61338ab2b1be607f85ca3f2a
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2024-01-08 23:17:03 +0600

    Navbar Modified

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,46 +1,64 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  const navOptions = (
-    <>
-      <li>
-        <a>Item 1</a>
-      </li>
-      <li>
-        <a>Parent</a>
-    
-      </li>
-    </>
-  );
   return (
-    <div className="navbar bg-base-100 justify-between">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        {/* Replace the link and logo with your actual logo and home page link */}
+        <Link to="/" className="btn btn-ghost text-xl">
+          <img
+            src="logo.jpg"
+            alt="Your Logo"
+            className="h-8 w-8 mr-2"
+          />
+          Cat Adoption
+        </Link>
+      </div>
+      <div className="flex-none">
+        <div className="space-x-4">
+          {/* Add relevant links to navigate to different sections */}
+          <Link to="/home" className="btn btn-ghost">
+            Home
+          </Link>
+          <Link to="/pet-listing" className="btn btn-ghost">
+            Pet Listing
+          </Link>
+          <Link to="/donation-campaigns" className="btn btn-ghost">
+            Donation Campaigns
+          </Link>
+          <Link to="/login-register" className="btn btn-ghost">
+            Login/Register
+          </Link>
+        </div>
+        <div className="dropdown dropdown-end">
+          {/* Use your user profile picture */}
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="User Profile"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
               />
-            </svg>
+            </div>
           </div>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            {navOptions}
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link>Settings</Link>
+            </li>
+            <li>
+              <Link>Logout</Link>
+            </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{navOptions}</ul>
       </div>
     </div>
   );
